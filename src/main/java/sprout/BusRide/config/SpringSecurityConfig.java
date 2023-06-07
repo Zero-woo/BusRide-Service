9907d5a55affea5d24ff5a266390c045e9adae78
youngwoo <owq0522@gmail.com>
2023-06-07 21:49:01 +0900

    fix : 로그인 제거

--- src/main/java/sprout/BusRide/config/SpringSecurityConfig.java
+++ src/main/java/sprout/BusRide/config/SpringSecurityConfig.java
@@ -1,22 +1,22 @@
-package sprout.BusRide.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@EnableWebSecurity
-public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/api/auth/**");
-
-        http.authorizeRequests().anyRequest().authenticated();
-
-        http.csrf().disable();
-
-        http.formLogin();
-    }
-}
+//package sprout.BusRide.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests().antMatchers("/api/auth/**");
+//
+//        http.authorizeRequests().anyRequest().authenticated();
+//
+//        http.csrf().disable();
+//
+//        http.formLogin();
+//    }
+//}
